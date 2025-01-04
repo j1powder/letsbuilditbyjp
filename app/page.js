d7df4582996eb1be7885ddaf07c1462927166b8e
Joe Powder <j1powder@yahoo.com>
2025-01-04 08:09:54 -0700

    adding css

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import computer from '../public/a_computer_being_used.jpeg';
 import purple from '../public/purple.jpg';
 import banner from '../public/banner.jpg';
 import green from '../public/greenComputers.png'
+import seoContent from '../public/seoContent.jpeg'
 import { Container, Row, Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
@@ -23,7 +24,20 @@ export default function Home() {
       <div >
       <h1 className={styles.affordable}>Responsive, User-Friendly Websites That Drive Results
       </h1>
-      <Row>
+      
+
+          <div className={styles.container}>
+      <div className={styles.div1}><p>Let’s Build It By JP provides website design with purpose and practicality in mind. Websites can be built to look cool and have a lot of neat features, , but what’s the point if no one knows you’re online?</p>
+      <p>Your website can have amazing photos and look stunning, but if it takes 5 minutes to load, you won’t have a lot of customers tolerating that long load time.</p>
+      <p>Let’s Build It By JP LLC builds FAST websites with purpose and practicality in mind! Not only that, we build your website at a price you can afford!</p>
+      </div>
+      <div className={styles.div2}>        
+         <Image className={styles.seoCont}  src={computer} alt="A computer being used" /> 
+      </div>
+    </div>
+
+
+        <Row>
         <Col sm={6}>
         <Card style={{padding: '4rem'}}>
         <p>Let’s Build It By JP provides website design with purpose and practicality in mind. Websites can be built to look cool and have a lot of neat features, , but what’s the point if no one knows you’re online?</p>
@@ -45,6 +59,18 @@ export default function Home() {
       <p>JP</p>
       <p>Owner- Let's Build It By JP LLC</p>
       </div>
+      <br/>
+      <div className={styles.container}>
+      <div className={styles.div1}><p>Let’s Build It By JP provides website design with purpose and practicality in mind. Websites can be built to look cool and have a lot of neat features, , but what’s the point if no one knows you’re online?</p>
+      <p>Your website can have amazing photos and look stunning, but if it takes 5 minutes to load, you won’t have a lot of customers tolerating that long load time.</p>
+      <p>Let’s Build It By JP LLC builds FAST websites with purpose and practicality in mind! Not only that, we build your website at a price you can afford!</p>
+      </div>
+      <div className={styles.div2}>        
+         <Image className={styles.seoCont}  src={computer} alt="A computer being used" /> 
+      </div>
+    </div>
+    <br/>
+    <br/>
       </Container>
 
     </Fragment>
